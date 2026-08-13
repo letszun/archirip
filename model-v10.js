4827f987080c844d772a54c64bc494c370741e26
letszun <letszun@gmail.com>
2026-08-13 15:59:53 +0900

    Add files via upload

---
+++ model-v10.js
@@ -0,0 +1,10 @@
+window.BOOK_MODEL_DATA = {
+  height: 3.15,
+  defaultAspect: 143 / 210,
+  referenceHeightMm: 210,
+  minThicknessMm: 8,
+  maxThicknessMm: 60,
+  coverInsetX: 0.018,
+  coverInsetY: 0.014,
+  perspectiveDistance: 12.5
+};
